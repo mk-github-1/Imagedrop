609f4f815f9fee49a8cb25fc9331ecd49f1214fe
yuchan <32920703+yuchan1@users.noreply.github.com>
2022-08-13 20:59:43 +0900

    Update imagedrop.js

--- imagedrop.js
+++ imagedrop.js
@@ -3,52 +3,6 @@
  *
  * Copyright 2021 yuchan1.
  * Licensed under the MIT license.
- *
- * (javascriptライブラリの説明)
- * これはformタグ下に、画像ファイルをドラッグ＆ドロップ、またはダブルクリックして選択するエリアを提供し、画像をアップロードをするためのjavascriptの軽量なライブラリです。
- *
- * (前提条件)
- * ・imagedrop.jsとimagedrop.cssのファイルを読み込みしてください。
- * ・ファイルアップロードのPost処理にはaxiosを使用しています。axiosのライブラリをインストールして下さい。
- * ・Google ChromeまたはMicrosoft Edgeで動作します。
- * ・CSRF対策用のコードを設定しないと動作しません。this.imagedrop.setRequestVerificationTokenIdName("__RequestVerificationToken")のように、フレームワークに合わせてidを指定して下さい。
- * ・upload時のサーバー側の戻り値として、成功時はfileNameを受け取り。失敗時はメッセージを受け取ってalert表示をするようにしています。サーバー側でも2重チェックをして下さい。
- * 
- * (使い方)
- * ・html
- * <html>
- *     <body>
- *         <div class="col-md-6">
- *             <div class="content-main">
- *                 <form id="imagedrop" class="imagedrop" enctype="multipart/form-data"></form>
- *                 <input type="hidden" id="transactionId" />
- *                 <input type="hidden" id="fileName" />
- *             </div>
- *         </div>
- *     <body>
- * </html>
- *
- * ・javascript
- * <script>
- *     // 通常
- *     const uploadUrl = "xxx";
- *     const transactionId = 1;
- *     let fileName = "xxx";
- * 
- *     this.imagedrop = new Imagedrop();
- *     this.imagedrop.setCheckedUser(true);
- *     this.imagedrop.setUploadUrl(uploadUrl);
- *     this.imagedrop.setDirectoryPath("xxx");
- *     this.imagedrop.setRequestVerificationTokenName("__RequestVerificationToken");
- *
- *     // setTransactionは必要時のみ。
- *     this.imagedrop.setTransactionIdPropertyName("xxx");
- *     this.imagedrop.setTransactionId(transactionId);
- *     
- *     // ファイル名の設定または取得
- *     this.imagedrop.setFileName(fileName);
- *     this.imagedrop.getFileName();
- * </script>
  */
 
 class Imagedrop {
